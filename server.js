2a78f5272548857201d499465220d0b6ebc5f60e
ayushmeena1712 <ayush.meena.cs@gmail.com>
2024-11-27 16:21:07 +0530

    added frontend files in the public

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
-const io = require('socket.io')(3000)
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-const users = {}
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server); // <-- Correct usage for CommonJS
 
-io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
+const users = {};
+
+// Serve static files
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  socket.on('new-user', (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+  });
+
+  socket.on('send-chat-message', (message) => {
+    socket.broadcast.emit('chat-message', {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
